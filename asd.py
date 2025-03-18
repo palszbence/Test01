94d65d887948672d98d958c668b2523eadab6e5c
pszb <pszb@irinyi-ref.hu>
2025-03-18 14:05:04 +0100

    FEJLESZTES

--- asd.py
+++ asd.py
@@ -2,5 +2,8 @@ print("Hello Git!")
 a=5
 b=10
 print(a+b)
+a=int(input("Kérel egy egész számot:"))
+b=int(input("Kérel egy egész számot:"))
+print(f"ÖSSZEG:{a+b}")
 for i in range(1,3):
     print ("ASD")
